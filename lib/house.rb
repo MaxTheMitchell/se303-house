94d43e1ebabb95e67a7d9c909da4fb758a52f8d2
MaxTheMitchell <rat2043@gmail.com>
2021-05-12 18:08:47 -0700

    rename 'before the house' to 'things_around_the_house'

--- lib/house.rb
+++ lib/house.rb
@@ -7,37 +7,37 @@ class House
   end
 
   def line(number)
-    "This is #{before_the_house(number)}"
+    "This is #{things_around_the_house(number)}"
   end
 
   private
 
-  def before_the_house(number)
+  def things_around_the_house(number)
     case number
     when 1
       "the house that Jack built.\n"
     when 2
-      "the malt that lay in #{before_the_house(1)}"
+      "the malt that lay in #{things_around_the_house(1)}"
     when 3 
-      "the rat that ate #{before_the_house(2)}"
+      "the rat that ate #{things_around_the_house(2)}"
     when 4 
-      "the cat that killed #{before_the_house(3)}"
+      "the cat that killed #{things_around_the_house(3)}"
     when 5 
-      "the dog that worried #{before_the_house(4)}"
+      "the dog that worried #{things_around_the_house(4)}"
     when 6
-      "the cow with the crumpled horn that tossed #{before_the_house(5)}"
+      "the cow with the crumpled horn that tossed #{things_around_the_house(5)}"
     when 7
-      "the maiden all forlorn that milked #{before_the_house(6)}"
+      "the maiden all forlorn that milked #{things_around_the_house(6)}"
     when 8 
-      "the man all tattered and torn that kissed #{before_the_house(7)}"
+      "the man all tattered and torn that kissed #{things_around_the_house(7)}"
     when 9 
-      "the priest all shaven and shorn that married #{before_the_house(8)}"
+      "the priest all shaven and shorn that married #{things_around_the_house(8)}"
     when 10 
-      "the rooster that crowed in the morn that woke #{before_the_house(9)}"
+      "the rooster that crowed in the morn that woke #{things_around_the_house(9)}"
     when 11 
-      "the farmer sowing his corn that kept #{before_the_house(10)}"
+      "the farmer sowing his corn that kept #{things_around_the_house(10)}"
     when 12 
-      "the horse and the hound and the horn that belonged to #{before_the_house(11)}"
+      "the horse and the hound and the horn that belonged to #{things_around_the_house(11)}"
     end 
   end
 end
