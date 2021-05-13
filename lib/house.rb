3feed370c1fb55cbee749db784bfa7ed95f102cd
MaxTheMitchell <rat2043@gmail.com>
2021-05-12 18:26:22 -0700

    replace recurstion with slice and joining the list

--- lib/house.rb
+++ lib/house.rb
@@ -13,23 +13,19 @@ class House
   private
 
   def things_around_the_house(number)
-    if number == 0
-      ""
-    else 
-      [
-        "the house that Jack built.\n",
-        "the malt that lay in ",
-        "the rat that ate ",
-        "the cat that killed ",
-        "the dog that worried ",
-        "the cow with the crumpled horn that tossed ",
-        "the maiden all forlorn that milked ",
-        "the man all tattered and torn that kissed ",
-        "the priest all shaven and shorn that married ",
-        "the rooster that crowed in the morn that woke ",
-        "the farmer sowing his corn that kept ",
-        "the horse and the hound and the horn that belonged to "
-      ][number - 1] + things_around_the_house(number - 1)
-    end
+    [
+      "the house that Jack built.\n",
+      "the malt that lay in ",
+      "the rat that ate ",
+      "the cat that killed ",
+      "the dog that worried ",
+      "the cow with the crumpled horn that tossed ",
+      "the maiden all forlorn that milked ",
+      "the man all tattered and torn that kissed ",
+      "the priest all shaven and shorn that married ",
+      "the rooster that crowed in the morn that woke ",
+      "the farmer sowing his corn that kept ",
+      "the horse and the hound and the horn that belonged to "
+    ][0...number].reverse.join("")
   end
 end
