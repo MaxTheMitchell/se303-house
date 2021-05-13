23d7bcbf975b712a312600ee11f85867f0fd85eb
MaxTheMitchell <rat2043@gmail.com>
2021-05-12 17:49:50 -0700

    get 'when 9' to be idential to the else

--- lib/house.rb
+++ lib/house.rb
@@ -9,7 +9,7 @@ class House
   def line(number)
     case number
     when 9
-      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{before_the_house(number)}the house that Jack built.\n"
     when 10
       "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 11
@@ -41,6 +41,8 @@ class House
       "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     when 8 
       "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+    when 9 
+      "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     end 
   end
 end
